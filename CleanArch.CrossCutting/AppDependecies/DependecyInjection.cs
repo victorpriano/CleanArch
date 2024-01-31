399cee6314f7df866a0a132cc1cb4ac11fee8c11
Victor Cipriano <victorcuru@gmail.com>
2024-01-31 20:35:01 -0300

    feat: camada de injeção de dependência

---
+++ CleanArch.CrossCutting/AppDependecies/DependecyInjection.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Domain.Abstractions;
+using CleanArch.Infrastructure.Context;
+using CleanArch.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.CrossCutting.AppDependecies;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var pgsql = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(pgsql));
+
+        services.AddScoped<IMemberRepository, MemberRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
