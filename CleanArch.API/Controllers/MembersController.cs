b65441e4abcedf0bc35dd4b564496ed2a2359ed8
Victor Cipriano <victorcuru@gmail.com>
2024-02-01 21:40:06 -0300

    feat: endpoint de teste

---
+++ CleanArch.API/Controllers/MembersController.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Domain.Abstractions;
+using CleanArch.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class MembersController : ControllerBase
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public MembersController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetMembers()
+    {
+        var members = await _unitOfWork.MemberRepository.GetAll();
+
+        return Ok(members);
+    }
+    
+}
